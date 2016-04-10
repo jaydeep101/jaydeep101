f70c140686db4b9782d9bb4c628c66cf4b659890
jaydeep101 <atreonmusic@gmail.com>
2016-04-10 17:17:15 -0400

    music change

--- otherProjects/finalAssignment/js/final.js
+++ otherProjects/finalAssignment/js/final.js
@@ -1,15 +1,14 @@
 
-var x = document.getElementById("myAudio"); 
+var x = document.getElementById("myAudio");
 
-function playAudio() { 
-    x.play(); 
-    alert("Thunder Road by Bruce Springsteen is now playing");
- 
-} 
+function playAudio() {
+    x.play();
+    alert("8-Bit Music Now Playing");
 
-function pauseAudio() { 
-    x.pause(); 
+}
+
+function pauseAudio() {
+    x.pause();
     alert("Music is paused");
-  
-} 
- 
+
+}
